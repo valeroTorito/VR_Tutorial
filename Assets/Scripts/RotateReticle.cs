9c4b9ecc06580634a586ca813e1dad852929fe55
valeroTorito <o.foiaqa@gmail.com>
2023-10-10 16:48:29 +0300

    added teleportation
    teleport, teleport  area, script rotationReticle

---
+++ Assets/Scripts/RotateReticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateReticle : MonoBehaviour
+{
+    // Скорость вращения мишени
+    public float rotationSpeed = 50.0f;
+
+    void Update()
+    {
+        // Вращаем мишень вокруг оси Y
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+    }
+}
